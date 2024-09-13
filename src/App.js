4babbb8edf8a553738ff0bece6a5e2c7d02492e8
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-13 12:18:14 -0300

    Implement calculator interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Container,
+  Grid,
+  GridItem,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
 
 function App() {
+  const [currentNumber] = useState("0");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <Container borderRadius={60} bg="gray.300" my={20}>
+          <Box width="350px" margin="0 auto" padding="20px">
+            <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+              <GridItem colSpan={4}>
+                <Box bg="gray.200" h="50px" rounded={10}>
+                  <Text
+                    justifySelf="flex-end"
+                    align="end"
+                    px={5}
+                    fontSize="3xl"
+                    fontWeight={700}
+                  >
+                    {currentNumber}
+                  </Text>
+                </Box>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">7</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">8</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">9</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">/</Button>
+              </GridItem>
+
+              <GridItem>
+                <Button w="100%">4</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">5</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">6</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">*</Button>
+              </GridItem>
+
+              <GridItem>
+                <Button w="100%">1</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">2</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">3</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">-</Button>
+              </GridItem>
+
+              <GridItem colSpan={2}>
+                <Button w="100%">0</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">.</Button>
+              </GridItem>
+              <GridItem>
+                <Button w="100%">+</Button>
+              </GridItem>
+            </Grid>
+            <Button w="100%" mt="4" colorScheme="teal" size="lg">
+              =
+            </Button>
+          </Box>
+        </Container>
+      </ChakraProvider>
     </div>
   );
 }
